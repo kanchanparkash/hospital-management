dbf4d76502191a2be6f5830f4334c34d764ad8ab
kanchanparkash <bajajkanchan84@gmail.com>
2026-06-27 01:25:53 +0200

    green: implemented patient entity

---
+++ src/main/java/com/examples/hospital/model/Patient.java
@@ -0,0 +1,59 @@
+package com.examples.hospital.model;
+
+import java.util.Objects;
+
+public class Patient {
+	private String id;
+	private String name;
+
+	public Patient() {
+
+	}
+
+	public Patient(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Patient other = (Patient) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + ", name=" + name + "]";
+	}
+
+}
